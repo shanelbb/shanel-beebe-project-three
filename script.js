bae4210857f40884cdf868a0bd128b9e36b8ed54
Shanel Beebe <shanelbeebe@icloud.com>
2020-03-11 21:04:32 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// GUESS THAT COLOUR GAME
+/* This program is a game that will allow a user to choose between 
+a set of coloured buttons to guess which colour corresponds with the
+displayed colour code. 
+
+1. Create a starting page with buttons representing the skill 
+level the user wants to play (easy, medium, hard) 
+2. If "easy" **** MVP *****
+        display RGB colour mode; 
+      if "medium" 
+        display HSL colour mode;
+      if "hard"  **** STRETCH GOAL *****
+        display HEX colour mode;  
+3. make a function that sets the color of each selection button.
+4. make a function that sets the colour value randomly.
+    - function 1 : generates RGB code *** MVP ***
+    - function 2 : generates HSL code
+    - function 3 : generates HEX code *** STRETCH GOAL ***
+5. make a function that starts the game in the preferred mode
+6. make one of the generated colours match the displayed color code
+7. add an event listener so when the color buttons are pressed the 
+user gets a message saying either correct or wrong. 
+8. If correct set the background color of the page to correct color
+9. If wrong display a message like "Wrong. Try Again" 
+10. On game page have 1 button that let's user guess again and 1 
+button that let user reset the game to beginning difficulty choices
+
+**************** STRETCH GOALS *********************
+1. Make a score counter function
+2. Make a function that causes the correct answer to be revealed when user guesses the wrong choice. 
+3. Add a button that lets user move on to next set of colours. 
+4. Have final score display after 10 guesses 
+ 5. Have button to reset game to beginning
+
+
