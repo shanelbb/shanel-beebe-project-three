462136559d1a28bfd75d92c1bde20157f8d9ba57
Shanel Beebe <shanelbeebe@icloud.com>
2020-03-15 13:46:53 -0400

    refactored some code and working on score function

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
-/* 
-GUESS THAT COLOUR GAME
-
-This program is a game that will allow a user to choose between 
-a set of coloured buttons to guess which colour corresponds with the
-displayed colour code. 
-
-1. Create a starting page with buttons representing the skill 
-level the user wants to play (easy, medium, hard) 
-
-2. Make function that brings up a start page when one of the level button is pressed.
-*/
-
-/* 3. If "easy" **** MVP *****
-        display RGB colour mode;
-      if "medium"
-        display HSL colour mode;
-      if "hard"  **** STRETCH GOAL *****
-      display HEX colour mode;  
-
- 4. make a function that sets the color of each selection button.*/
-
-/*5. make a function that sets the colour value randomly.
-    - function 1 : generates RGB code *** MVP ***
-    - function 2 : generates HSL code
-    - function 3 : generates HEX code *** STRETCH GOAL ***
-6. make a function that starts the game in the preferred mode
-7. make one of the generated colours match the displayed color code
-8. add an event listener so when the color buttons are pressed the 
-user gets a message saying either correct or wrong. 
-9. If correct set the background color of the page to correct color
-10. If wrong display a message like "Wrong. Try Again" 
-11. On game page have 1 button that let's user guess again and 1 
-button that let user reset the game to beginning difficulty choices
-
+/*
 **************** STRETCH GOALS *********************
 1. Make a score counter function
 2. Make a function that causes the correct answer to be revealed when user guesses the wrong choice. 
@@ -54,10 +20,14 @@ $(function() {
   const $answerMessage = $("#answer");
   const $gamePage = $(".gamePage");
   const $startPage = $(".startPage");
+  const $next = $(".nextQuestion");
+  const $final = $(".finalScore");
+  const $score = $(".finalScoreMessage");
 
   // keeps gamePage hiddend until level button is clicked
   const landingPage = function() {
     $gamePage.hide();
+    $final.hide();
   };
 
   // resets background to original color
@@ -70,12 +40,60 @@ $(function() {
     $answerMessage.html("Choose carefully!");
   };
 
-  // resets game to startPage
-  const reset = () => {
-    $gamePage.hide();
-    $startPage.show();
+  // resets background color and answer message
+  const resetPlay = () => {
     resetBackGround();
     answerMessageReset();
+  };
+
+  // resets game to startPage
+  const reset = () => {
+    landingPage();
+    $startPage.show();
+    resetPlay();
+    resetScore();
+    resetCounter();
+  };
+
+  let score = 0;
+
+  const resetScore = () => {
+    score = 0;
+    return score;
+  };
+
+  const incrementScore = num => {
+    score += num;
+    console.log("score", score);
+    return score;
+  };
+
+  $(".restart").on("click", function() {
+    reset();
+  });
+
+  const maxQuestions = 10;
+  let questionCounter = 0;
+
+  const endGame = () => {
+    if (questionCounter === maxQuestions) {
+      $gamePage.hide();
+      $final.show();
+      $score.html(`Your final score is: </br>
+      ${score} / 10`);
+    }
+  };
+
+  const setNextQ = () => {
+    resetPlay();
+    questionCounter++;
+    console.log("QC:", questionCounter);
+    endGame();
+  };
+
+  const resetCounter = () => {
+    questionCounter = 0;
+    return questionCounter;
   };
 
   // sets the game up in rgb/easy mode
@@ -107,20 +125,19 @@ $(function() {
 
       if (i === answerButton) {
         $colorValue.html(`Colour Code: </br>
-      rgb(${red}, ${green}, ${blue})`);
+     rgb(${red}, ${green}, ${blue})`);
       }
 
-      // function that displays "Correct" or "Wrong" based on user input. Also changes background to correct answer colour.
+      // event handler that displays "Correct" or "Wrong" based on user input. Also changes background to correct answer colour.
       $($buttons[i]).on("click", function() {
         if (this === $buttons[answerButton]) {
           $answerMessage.html("Correct!");
-          $background.attr(
-            `style`,
-            `background-color: rgb(${red}, ${green}, ${blue})`
-          );
+          $background.css(`background-color`, `rgb(${red}, ${green}, ${blue})`);
+          incrementScore(1);
         } else {
           $answerMessage.html("Wrong answer! Guess again.");
           resetBackGround();
+          incrementScore(0);
         }
       });
     }
@@ -159,19 +176,21 @@ $(function() {
 
       if (i === answerButton) {
         $colorValue.html(`Colour Code: </br> 
-      hsl(${hue}, ${saturation}%, ${light}%)`);
+     hsl(${hue}, ${saturation}%, ${light}%)`);
       }
 
       $($buttons[i]).on("click", function() {
         if (this === $buttons[answerButton]) {
           $answerMessage.html("Correct!");
-          $background.attr(
-            `style`,
-            `background-color: hsl(${hue}, ${saturation}%, ${light}%)`
+          $background.css(
+            `background-color`,
+            `hsl(${hue}, ${saturation}%, ${light}%)`
           );
+          incrementScore(1);
         } else {
           $answerMessage.html("Wrong answer! Guess again.");
           resetBackGround();
+          incrementScore(0);
         }
       });
     }
@@ -201,16 +220,18 @@ $(function() {
 
       if (i === answerButton) {
         $colorValue.html(`Colour Code: </br> 
-      ${hexVal}`);
+     ${hexVal}`);
       }
 
       $($buttons[i]).on("click", function() {
         if (this === $buttons[answerButton]) {
           $answerMessage.html("Correct!");
-          $background.attr(`style`, `background-color: ${hexVal}`);
+          $background.css(`background-color`, `${hexVal}`);
+          incrementScore(1);
         } else {
           $answerMessage.html("Wrong answer! Guess again.");
           resetBackGround();
+          incrementScore(0);
         }
       });
     }
@@ -226,28 +247,25 @@ $(function() {
 
     $(".easy").on("click", function() {
       rgbMode();
-      $(".playAgain").on("click", function() {
+      $next.on("click", function() {
         rgbMode();
-        resetBackGround();
-        answerMessageReset();
+        setNextQ();
       });
     });
 
     $(".medium").on("click", function() {
       hslMode();
-      $(".playAgain").on("click", function() {
+      $next.on("click", function() {
         hslMode();
-        resetBackGround();
-        answerMessageReset();
+        setNextQ();
       });
     });
 
     $(".hard").on("click", function() {
       hexMode();
-      $(".playAgain").on("click", function() {
+      $next.on("click", function() {
         hexMode();
-        resetBackGround();
-        answerMessageReset();
+        setNextQ();
       });
     });
 
