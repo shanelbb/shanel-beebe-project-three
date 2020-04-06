10286d7c94b108a5b8d62679a8c501048b13736e
Shanel Beebe <shanelbeebe@icloud.com>
2020-04-06 17:55:05 -0400

    refactored code to namespacing pattern

--- script.js
+++ script.js
@@ -1,109 +1,100 @@
 const app = {};
 
-// cached selectors
-const $answerMessage = $("#answer");
-const $background = $("body");
-const $buttons = $(".colourButton");
-const $colorValue = $("#colourValue");
-const $final = $(".finalScore");
-const $gamePage = $(".gamePage");
-const $label = $("label");
-const $next = $(".nextQuestion");
-const $score = $(".finalScoreMessage");
-const $startPage = $(".startPage");
-
 // **** APP STARTING PAGE ****
-app.landingPage = function() {
-  $gamePage.hide();
-  $final.hide();
+app.landingPage = function () {
+  app.gamePage.hide();
+  app.final.hide();
 };
 
 // resets background to original color
-const resetBackGround = () => {
-  $background.attr("style", "background-color: #4aafbc");
+app.resetBackground = () => {
+  app.background.attr("style", "background-color: #4aafbc");
 };
 
-// resets answerMessage under color $buttons to original message
-const answerMessageReset = () => {
-  $answerMessage.html("Choose carefully!");
+// resets answerMessage under color app.buttons to original message
+app.answerMessageReset = () => {
+  app.answerMessage.html("Choose carefully!");
+  app.answerMessage.removeClass("result");
 };
 
 // resets the colour name label to blank between each turn in named mode
-const nameReset = () => {
-  $label.html("");
+app.nameReset = () => {
+  app.label.html("");
 };
 
 // resets background color and answer message
-const resetPlay = () => {
-  resetBackGround();
-  answerMessageReset();
-  nameReset();
+app.resetPlay = () => {
+  app.resetBackground();
+  app.answerMessageReset();
+  app.nameReset();
 };
 
 // starts score at 0
-let score = 0;
+app.score = 0;
 
-const resetScore = () => {
-  score = 0;
-  return score;
+app.resetScore = () => {
+  app.score = 0;
+  return app.score;
 };
 
 // Sets max number of questions played per round
-const maxQuestions = 10;
-let questionCounter = 0;
+app.maxQuestions = 10;
+app.questionCounter = 0;
 
 // Move user to next question. Called when "next color" button is clicked
-const setNextQ = () => {
-  resetPlay();
-  questionCounter++;
-  endGame();
+app.setNextQ = () => {
+  app.resetPlay();
+  app.questionCounter++;
+  app.endGame();
 };
 
 // Function called when the correct answer is selected in each mode
-const correctAnswer = function(colorValue) {
-  score++;
-  $answerMessage.html("Correct!");
-  $background.css(`background-color`, colorValue); // changes bg colour to correct colour
-  $buttons.css(`background-color`, colorValue); //changes all colour buttons to the correct colour
-  $buttons.off("click"); // disables all answer buttons once one answer has been submitted
+app.correctAnswer = function (colorValue) {
+  app.score++;
+  app.answerMessage.html("Correct!");
+  app.answerMessage.addClass("result");
+  app.background.css(`background-color`, colorValue); // changes bg colour to correct colour
+  app.buttons.css(`background-color`, colorValue); //changes all colour buttons to the correct colour
+  app.buttons.off("click"); // disables all answer buttons once one answer has been submitted
 };
 
 // wrong answer function
-const wrongAnswer = () => {
-  $answerMessage.html("Wrong answer!");
-  resetBackGround();
-  $buttons.off("click"); // disables all answer buttons once one answer has been submitted
+app.wrongAnswer = () => {
+  app.answerMessage.html("Wrong answer");
+  app.answerMessage.addClass("result");
+  app.resetBackground();
+  app.buttons.off("click"); // disables all answer buttons once one answer has been submitted
 };
 
 // ends game after user answers 10 questions; displays final score screen
-const endGame = () => {
-  if (questionCounter === maxQuestions) {
-    $gamePage.hide();
-    $final.show();
-    $score.html(`Your final score is:
-    <span class="bold">${score} / 10</span>`);
+app.endGame = () => {
+  if (app.questionCounter === app.maxQuestions) {
+    app.gamePage.hide();
+    app.final.show();
+    app.finalScore.html(`Your final score is:
+    <span class="bold">${app.score} / 10</span>`);
   }
 };
 
-// resets questionCounter to 0
-const resetCounter = () => {
-  questionCounter = 0;
-  return questionCounter;
+// resets app.questionCounter to 0 when user starts game over
+app.resetCounter = () => {
+  app.questionCounter = 0;
+  return app.questionCounter;
 };
 
 // **** RESETS GAME TO START PAGE *****
 app.reset = () => {
   app.landingPage();
-  $startPage.show();
-  resetPlay();
-  resetScore();
-  resetCounter();
+  app.startPage.show();
+  app.resetPlay();
+  app.resetScore();
+  app.resetCounter();
 };
 
 //  ****** SETS UP GAME IN RGB MODE *******
-const rgbMode = function() {
+app.rgbMode = function () {
   // sets the button color using an rgb value
-  const setRGBButtonColour = function(button, red, green, blue) {
+  const setRGBButtonColour = function (button, red, green, blue) {
     $(button).css(`background-color`, `rgb(${red}, ${green}, ${blue})`);
   };
 
@@ -113,33 +104,33 @@ const rgbMode = function() {
   };
 
   // generates a random numer between 0 and 3 or number of buttons - 1. ** Must be inside this function to keep the correct answer random
-  const answerButton = Math.floor(Math.random() * ($buttons.length - 1));
+  const answerButton = Math.floor(Math.random() * (app.buttons.length - 1));
 
-  for (let i = 0; i < $buttons.length; i++) {
+  for (let i = 0; i < app.buttons.length; i++) {
     // loops through each button to set them each to a random rgb value
     const red = makeRGBValue();
     const green = makeRGBValue();
     const blue = makeRGBValue();
-    setRGBButtonColour($buttons[i], red, green, blue);
+    setRGBButtonColour(app.buttons[i], red, green, blue);
 
-    // if value of answerButton equals index of $buttons appends the corresponding colour code to the h2 with the class of colorValue
+    // if value of answerButton equals index of app.buttons appends the corresponding colour code to the h2 with the class of colorValue
     if (i === answerButton) {
-      $colorValue.html(`Guess which colour matches this colour code:
+      app.colourValue.html(`Guess which colour matches this colour code:
   <span class="bold">rgb(${red}, ${green}, ${blue})</span>`);
     }
 
     // event handler to take in user guess and respond according to whether they are right or wrong
-    $($buttons[i])
+    $(app.buttons[i])
       .off() // .off keeps click from firing multiple times, .one makes correct answer only clickable once to accumulate a point.
-      .one("click", function() {
-        if (this === $buttons[answerButton]) {
-          correctAnswer(`rgb(${red}, ${green}, ${blue})`);
+      .one("click", function () {
+        if (this === app.buttons[answerButton]) {
+          app.correctAnswer(`rgb(${red}, ${green}, ${blue})`);
         } else {
-          wrongAnswer();
+          app.wrongAnswer();
           // change all colour buttons to correct colour
-          $buttons.css(
+          app.buttons.css(
             `background-color`,
-            `${$buttons[answerButton].style.backgroundColor}`
+            `${app.buttons[answerButton].style.backgroundColor}`
           );
         }
       });
@@ -147,9 +138,9 @@ const rgbMode = function() {
 };
 
 //  ****** SETS UP GAME IN HSL MODE *******
-const hslMode = function() {
+app.hslMode = function () {
   // sets the button color using an hsl value
-  const setHSLButtonColour = function(button, h, s, l) {
+  const setHSLButtonColour = function (button, h, s, l) {
     $(button).css(`background-color`, `hsl(${h}, ${s}%, ${l}%)`);
   };
 
@@ -164,33 +155,33 @@ const hslMode = function() {
   };
 
   // generates a random numer between 0 and 3 or number of buttons - 1. ** Must be inside this function to keep the correct answer random
-  const answerButton = Math.floor(Math.random() * ($buttons.length - 1));
+  const answerButton = Math.floor(Math.random() * (app.buttons.length - 1));
 
-  for (let i = 0; i < $buttons.length; i++) {
+  for (let i = 0; i < app.buttons.length; i++) {
     // loops through each button to set them each to a random hsl value
     const hue = makeHValue();
     const saturation = makeSLValues();
     const light = makeSLValues();
-    setHSLButtonColour($buttons[i], hue, saturation, light);
+    setHSLButtonColour(app.buttons[i], hue, saturation, light);
 
-    // if value of answerButton equals index of $buttons appends the corresponding colour code to the h2 with the class of colorValue
+    // if value of answerButton equals index of app.buttons appends the corresponding colour code to the h2 with the class of colorValue
     if (i === answerButton) {
-      $colorValue.html(`Guess which colour matches this colour code:  
+      app.colourValue.html(`Guess which colour matches this colour code:  
    <span class="bold">hsl(${hue}, ${saturation}%, ${light}%)</span>`);
     }
 
     // event handler to take in user guess and respond according to whether they are right or wrong
-    $($buttons[i])
+    $(app.buttons[i])
       .off() // .off keeps click from firing multiple times, .one makes correct answer only clickable once to accumulate a point.
-      .one("click", function() {
-        if (this === $buttons[answerButton]) {
-          correctAnswer(`hsl(${hue}, ${saturation}%, ${light}%)`);
+      .one("click", function () {
+        if (this === app.buttons[answerButton]) {
+          app.correctAnswer(`hsl(${hue}, ${saturation}%, ${light}%)`);
         } else {
-          wrongAnswer();
+          app.wrongAnswer();
           // change all colour buttons to correct colour
-          $buttons.css(
+          app.buttons.css(
             `background-color`,
-            `${$buttons[answerButton].style.backgroundColor}`
+            `${app.buttons[answerButton].style.backgroundColor}`
           );
         }
       });
@@ -198,9 +189,9 @@ const hslMode = function() {
 };
 
 // ****** SETS UP GAME IN HEX MODE *******
-const hexMode = function() {
+app.hexMode = function () {
   // sets the button color using a hex value
-  const setHexButtonColour = function(button, hex) {
+  const setHexButtonColour = function (button, hex) {
     $(button).css(`background-color`, `${hex}`);
   };
 
@@ -216,31 +207,31 @@ const hexMode = function() {
   };
 
   // generates a random numer between 0 and 3 or number of button - 1. ** Must be inside this function to keep the correct answer random
-  const answerButton = Math.floor(Math.random() * ($buttons.length - 1));
+  const answerButton = Math.floor(Math.random() * (app.buttons.length - 1));
 
-  for (let i = 0; i < $buttons.length; i++) {
+  for (let i = 0; i < app.buttons.length; i++) {
     // loops through each button to set them each to a random hex value
     const hexVal = makeHexValue();
-    setHexButtonColour($buttons[i], hexVal);
+    setHexButtonColour(app.buttons[i], hexVal);
 
-    // if value of answerButton equals index of $buttons appends the corresponding colour code to the h2 with the class of colorValue
+    // if value of answerButton equals index of app.buttons appends the corresponding colour code to the h2 with the class of colorValue
     if (i === answerButton) {
-      $colorValue.html(`Guess which colour matches this colour code: 
+      app.colourValue.html(`Guess which colour matches this colour code: 
    <span class="bold">Hex ${hexVal}</span>`);
     }
 
     // event handler to take in user guess and respond according to whether they are right or wrong
-    $($buttons[i])
+    $(app.buttons[i])
       .off() //.off keeps click from firing multiple times, .one makes correct answer only clickable once to accumulate a point.
-      .one("click", function() {
-        if (this === $buttons[answerButton]) {
-          correctAnswer(`${hexVal}`);
+      .one("click", function () {
+        if (this === app.buttons[answerButton]) {
+          app.correctAnswer(`${hexVal}`);
         } else {
-          wrongAnswer();
+          app.wrongAnswer();
           // change all colour buttons to correct colour
-          $buttons.css(
+          app.buttons.css(
             `background-color`,
-            `${$buttons[answerButton].style.backgroundColor}`
+            `${app.buttons[answerButton].style.backgroundColor}`
           );
         }
       });
@@ -248,7 +239,7 @@ const hexMode = function() {
 };
 
 // ********** SETS UP GAME IN NAMED MODE *********
-const namedMode = function() {
+app.namedMode = function () {
   // generates a random hex value to use with the endpoint url to get button colours from api
   const makeHexValue = () => {
     let hexCode = "";
@@ -261,59 +252,59 @@ const namedMode = function() {
   };
 
   // sets button colours using rgb values taken from the api call results
-  const setButtonColour = function(button, red, green, blue) {
+  const setButtonColour = function (button, red, green, blue) {
     $(button).css("background-color", `rgb(${red}, ${green}, ${blue})`);
   };
 
   // adds name of colour to the label beneath each button
-  const setButtonName = function(el, name) {
+  const setButtonName = function (el, name) {
     el.append(name);
   };
 
   // generates a random numer between 0 and 3 or number of buttons - 1. ** Must be inside this function to keep the correct answer random
-  const answerButton = Math.floor(Math.random() * ($buttons.length - 1));
+  const answerButton = Math.floor(Math.random() * (app.buttons.length - 1));
 
   // api url
   const endpoint = "https://api.color.pizza/v1/";
 
   // what to do in case of error from api call
   function handleError(err) {
-    setButtonName($label, `<label>(${err.statusText}</label>)`);
+    setButtonName(app.label, `<label>(${err.statusText}</label>)`);
   }
 
-  for (let i = 0; i < $buttons.length; i++) {
+  for (let i = 0; i < app.buttons.length; i++) {
     // Loops through each button to set a button colour and name taken from the api
     $.ajax({
       url: `${endpoint}${makeHexValue()}?noduplicates=true`,
       dataType: "json",
-      method: "GET"
+      method: "GET",
     })
-      .then(function(res) {
+      .then(function (res) {
         const html = `${res.colors[0].name}`;
-        setButtonName($label[i], html);
+        setButtonName(app.label[i], html);
         const r = res.colors[0].rgb.r;
         const g = res.colors[0].rgb.g;
         const b = res.colors[0].rgb.b;
-        setButtonColour($buttons[i], r, g, b);
+        setButtonColour(app.buttons[i], r, g, b);
 
-        // if value of answerButton equals index of $buttons appends the corresponding colour code to the h2 with the class of colorValue
+        // if value of answerButton equals index of app.buttons appends the corresponding colour code to the h2 with the class of colorValue
         if (i === answerButton) {
-          $colorValue.html(`Guess which colour matches this colour code:
+          app.colourValue.html(`Guess which colour matches this colour code:
   <span class="bold">rgb(${r}, ${g}, ${b})</span>`);
         }
 
         // event handler to take in user guess and respond according to whether they are right or wrong
-        $($buttons[i])
+        $(app.buttons[i])
           .off() // .off keeps click from firing multiple times, .one makes correct answer only clickable once to accumulate a point.
-          .one("click", function() {
-            if (this === $buttons[answerButton]) {
-              correctAnswer(`rgb(${r}, ${g}, ${b})`);
+          .one("click", function () {
+            if (this === app.buttons[answerButton]) {
+              app.correctAnswer(`rgb(${r}, ${g}, ${b})`);
             } else {
-              wrongAnswer();
+              app.wrongAnswer();
               // change all colour buttons to correct colour
-              $buttons.css(
+              app.buttons.css(
                 `background-color`,
-                `${$buttons[answerButton].style.backgroundColor}`
+                `${app.buttons[answerButton].style.backgroundColor}`
               );
             }
           });
@@ -330,7 +321,7 @@ app.modal = () => {
   const $close = $(".close");
 
   // opens the modal when the user clicks on the button
-  $("#infoBtn").on("click", function() {
+  $("#infoBtn").on("click", function () {
     $modal.css("display", "block");
   });
 
@@ -347,7 +338,7 @@ app.modal = () => {
   $close.on("keyup", handleClose);
 
   // closes the modal when the user clicks anywhere outside of it
-  $(window).click(function(e) {
+  $(window).click(function (e) {
     if ($(e.target).is($modal)) {
       $modal.css("display", "none");
     }
@@ -357,64 +348,77 @@ app.modal = () => {
 // ****** STARTS GAME WHEN LEVELE BUTTON IS CLICKED *******
 app.startGame = () => {
   //sets the answer message to "choose carefully"
-  answerMessageReset();
+  app.answerMessageReset();
 
   // hides the landing page and reveals the game page
-  $(".levelButton").on("click", function() {
-    $startPage.hide();
-    $gamePage.show();
+  $(".levelButton").on("click", function () {
+    app.startPage.hide();
+    app.gamePage.show();
   });
 
   // Takes users to the named mode when the named button is clicked
-  $(".named").on("click", function() {
-    namedMode();
-    $next.off("click").on("click", function() {
-      namedMode();
-      setNextQ();
+  $(".named").on("click", function () {
+    app.namedMode();
+    app.next.off("click").on("click", function () {
+      app.namedMode();
+      app.setNextQ();
     });
   });
 
   // takes users to rgb mode of the game when rgb button is clicked
-  $(".rgb").on("click", function() {
-    rgbMode();
-    $next.off("click").on("click", function() {
-      rgbMode();
-      setNextQ();
+  $(".rgb").on("click", function () {
+    app.rgbMode();
+    app.next.off("click").on("click", function () {
+      app.rgbMode();
+      app.setNextQ();
     });
   });
 
   // takes users to hsl mode of the game when hsl button is clicked
-  $(".hsl").on("click", function() {
-    hslMode();
-    $next.off("click").on("click", function() {
-      hslMode();
-      setNextQ();
+  $(".hsl").on("click", function () {
+    app.hslMode();
+    app.next.off("click").on("click", function () {
+      app.hslMode();
+      app.setNextQ();
     });
   });
 
   // takes users to hex mode of the game when hex button is clicked
-  $(".hex").on("click", function() {
-    hexMode();
-    $next.off("click").on("click", function() {
-      hexMode();
-      setNextQ();
+  $(".hex").on("click", function () {
+    app.hexMode();
+    app.next.off("click").on("click", function () {
+      app.hexMode();
+      app.setNextQ();
     });
   });
 
-  // resets game to landing page when restart or start over buttons are clicked
+  // resets game to landing page when start agaiin or play again buttons are clicked
   $(".resetButton").on("click", app.reset);
 
   $(".restart").on("click", app.reset);
 };
 
 // game initilization function
-app.init = function() {
+app.init = function () {
+  // cached selectors
+  app.answerMessage = $("#answer");
+  app.background = $("body");
+  app.buttons = $(".colourButton");
+  app.colourValue = $("#colourValue");
+  app.final = $(".finalScore");
+  app.gamePage = $(".gamePage");
+  app.label = $("label");
+  app.next = $(".nextQuestion");
+  app.finalScore = $(".finalScoreMessage");
+  app.startPage = $(".startPage");
+
+  // functions
   app.landingPage();
   app.startGame();
   app.modal();
 };
 
 // document ready function
-$(function() {
+$(function () {
   app.init();
 });
